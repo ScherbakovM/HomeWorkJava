e2472e1ecc09be15c16ee6699b6e4634631e0916
Михаил <109952823+ScherbakovM@users.noreply.github.com>
2023-02-24 22:12:53 +0300

    sem3task2

---
+++ task2.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+//2) Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее ариф. из этого списка
+
+public class task2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> numsList = new ArrayList<Integer>(10);
+        Random randDigit = new Random(100);
+        for (int i = 0; i < 10; i++) {
+           numsList.add(randDigit.nextInt(100));
+           System.out.print(numsList.get(i) + " ");   
+        }
+        System.out.println("");
+        System.out.println("Максимальное значение : " + Collections.max(numsList));
+        System.out.println("Минимальное значение : " + Collections.min(numsList));
+        midl(numsList);
+    }
+
+    public static void midl(ArrayList<Integer> arr) {
+        int summ = 0;
+        int midl = arr.size();
+        for (int index = 0; index < arr.size(); index++) {
+            summ += arr.get(index);
+        }
+        System.out.println("Среднее арифметическое : " + summ / midl);    
+    }
+}
